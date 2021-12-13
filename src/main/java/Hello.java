cc8850a186902655a281a3b806ba32a2eb3f6403
YoungTigerAshok <94345732+YoungTigerAshok@users.noreply.github.com>
2021-12-13 11:57:52 +0530

    updated

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,7 +3,7 @@ class Hello
    public static void main(String args[])
    {
       System.out.println("Hello World..hello..modify");
- System.out.println("new chnages")
-    System.out.println("sri")
+ System.out.println("new chnages");
+    System.out.println("sri");
    }
 }
